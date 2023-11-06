4dd0499520f677fc8bd07cb702d480fa04dbe2f5
Vladislav Kovalenko <vkoval2002@gmail.com>
2023-11-06 17:31:47 +0300

    Add docker-compose files

--- src/main/kotlin/com/dawisto/dailyclownbot/DailyClownBotApplication.kt
+++ src/main/kotlin/com/dawisto/dailyclownbot/DailyClownBotApplication.kt
@@ -2,10 +2,37 @@ package com.dawisto.dailyclownbot
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.event.ContextRefreshedEvent
+import org.springframework.context.event.EventListener
+import org.springframework.core.env.ConfigurableEnvironment
+import org.springframework.core.env.MapPropertySource
+import org.springframework.core.env.PropertySource
 
 
 @SpringBootApplication
-class DailyClownBotApplication
+class DailyClownBotApplication {
+
+    @EventListener
+    fun handleContextRefreshed(event: ContextRefreshedEvent) {
+        val env = event.applicationContext.environment as ConfigurableEnvironment
+        env.propertySources
+            .stream()
+            .filter { ps: PropertySource<*>? -> ps is MapPropertySource }
+            .map<Set<String>> { ps: PropertySource<*> -> (ps as MapPropertySource).source.keys }
+            .flatMap<String> { obj: Set<String> -> obj.stream() }
+            .distinct()
+            .sorted()
+            .forEach { key: String? ->
+                println(
+                    String.format(
+                        "%s=%s", key, env.getProperty(
+                            key!!
+                        )
+                    )
+                )
+            }
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<DailyClownBotApplication>(*args)
